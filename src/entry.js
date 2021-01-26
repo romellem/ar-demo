d7df0a7b219b10344e4ecef2ac2ddb6893cc4f41
Matt Wade <matt.wade@designory.com>
2021-01-26 15:24:49 -0600

    Comment on UMD variable name

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,5 @@
 import { wrapGrid } from 'animate-css-grid';
+// window.animateCSSGrid.wrapGrid
 
 import './css/index.scss';
 
@@ -34,35 +35,6 @@ const once = (element, event_type, callback) => {
 ready(() => {
   const $ = (s, c) => (c ? c.querySelector(s) : document.querySelector(s));
   const $$ = (s, c) => (c ? c.querySelectorAll(s) : document.querySelectorAll(s));
-
-  // const grid = $('.grid');
-
-  // $$('.cell').forEach(cell => cell.addEventListener('click', function() {
-  //   if (this.classList.contains('selected')) {
-  //     this.classList.remove('selected');
-  //   } else {
-  //     $('.selected')?.classList.remove('selected');
-  //     this.classList.add('selected');
-  //   }
-  // }));
-
-  // // {
-  // //   // int: default is 0 ms
-  // //   stagger: 100,
-  // //   // int: default is 250 ms
-  // //   duration: 500,
-  // //   // string: default is 'easeInOut'
-  // //   easing: 'backInOut',
-  // //   // function: called with list of elements about to animate
-  // //   onStart: (animatingElementList)=> {},
-  // //   // function: called with list of elements that just finished animating
-  // //   // cancelled animations will not trigger onEnd
-  // //   onEnd: (animatingElementList)=> {}
-  // // }
-
-  // window.animateCSSGrid.wrapGrid(grid, {
-  //   duration: 300,
-  // });
 
   function initTabs(initial_id = 1) {
     const tabs = $$('.tabs');
